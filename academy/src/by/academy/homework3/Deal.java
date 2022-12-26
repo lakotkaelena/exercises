30c6149b22ccec4808b06eb4a1581b0653ad25ac
Elena Lakotka <lakotkaelena8@gmail.com>
2022-12-26 15:14:48 +0300

    Task1

--- academy/src/by/academy/homework3/Deal.java
+++ academy/src/by/academy/homework3/Deal.java
@@ -32,23 +32,22 @@ public class Deal {
 	public void choiceProduct() {
 		Scanner scanner = new Scanner(System.in);
 		do {
-			System.out.println("Хотите выбрать товар?" + "\n 1. Да" + "\n 2. Нет"); // предлагаем пользователю выбрать
-																					// товар,
+			System.out.println("Хотите выбрать товар?" + "\n 1. Да" + "\n 2. Нет");
+
 			int ch = scanner.nextInt();
-			if (ch == 2) { // если нет, то формируем корзину выбранных товаров, выводим чек
+			if (ch == 2) {
 				printBasket(bayer.getProducts());
 				break;
 			}
-			System.out.println("Выберите товар из списка: "); // если да,предлагаем список товаров
+			System.out.println("Выберите товар из списка: ");
 			System.out.println(" Наименование:" + "\t" + "Количество:");
 			for (int i = 0; i < seller.getProducts().length; i++) {
 
 				System.out.println(i + 1 + ")" + seller.getProducts()[i].getName() + "  \t  "
 						+ seller.getProducts()[i].getQuantity());
 			}
-			int productPosition = scanner.nextInt(); // позиция товара в списке, выбранная пользователем
-			Product selected = seller.getProducts()[productPosition - 1]; // выбранный пользователем продукт извлекаемиз
-																			// списка
+			int productPosition = scanner.nextInt();
+			Product selected = seller.getProducts()[productPosition - 1];
 
 			if (selected.getQuantity() == 0) {
 				System.out.println("Данный товар закончился");
@@ -58,13 +57,13 @@ public class Deal {
 			System.out.println("Введите количество товара: ");
 			ch = scanner.nextInt();
 
-			while (ch > selected.getQuantity()) { // проверяем введенное количество
+			while (ch > selected.getQuantity()) {
 				System.out.println("Вы ввели количество больше " + selected.getQuantity() + ". Повторите ввод: ");
 				ch = scanner.nextInt();
 			}
 
 			boolean isAddProductBasket = true;
-			for (int i = 0; i < bayer.getProducts().length; i++) { // проверяем, есть ли уже товар с таким наименованием
+			for (int i = 0; i < bayer.getProducts().length; i++) {
 
 				if (bayer.getProducts()[i].getName().equals(selected.getName())) {
 					if (ch <= selected.getQuantity()) {
@@ -79,18 +78,13 @@ public class Deal {
 				}
 
 			}
-			if (isAddProductBasket) { // если такого товара нет в корзине, то
-				bayer.setProducts(Arrays.copyOf(bayer.getProducts(), bayer.getProducts().length + 1)); // пересоздаем
-																										// корзину с
-																										// увеличением
-																										// на 1 размера
+			if (isAddProductBasket) {
+				bayer.setProducts(Arrays.copyOf(bayer.getProducts(), bayer.getProducts().length + 1));
+
 				selected.setQuantity(selected.getQuantity() - ch);
-				selected.setSelectedCount(ch);// записали в объект введенное количство товара
-				bayer.getProducts()[bayer.getProducts().length - 1] = seller.getProducts()[productPosition - 1]; // добавляем
-																													// в
-																													// корзину
-																													// выбранный
-																													// продукт
+				selected.setSelectedCount(ch);
+				bayer.getProducts()[bayer.getProducts().length - 1] = seller.getProducts()[productPosition - 1];
+
 			}
 
 		} while (true);
