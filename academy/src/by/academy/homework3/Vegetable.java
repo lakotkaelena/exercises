93bbd978f19b6a5ecd182146a3260667a9e16817
Elena Lakotka <lakotkaelena8@gmail.com>
2022-12-26 14:59:50 +0300

    Task1-9

--- academy/src/by/academy/homework3/Vegetable.java
+++ academy/src/by/academy/homework3/Vegetable.java
@@ -1,16 +1,10 @@
 package by.academy.homework3;
 
-
-
 public class Vegetable extends Product {
-	
 
-	public Vegetable(String name, double price,int quantity) {
-		super( "Овощи", name, price,quantity,10);
+	public Vegetable(String name, double price, int quantity) {
+		super("Овощи", name, price, quantity, 10);
 
 	}
-
-
-	
 
 }
